3cbdb890b988b0c39dbbb0e28d6a7665d5813a1f
Robert <robert0714@gmail.com>
2016-10-12 23:57:08 -0500

    Update bootstrap4CentOs_ansible.sh

--- bootstrap4CentOs_ansible.sh
+++ bootstrap4CentOs_ansible.sh
@@ -3,16 +3,7 @@
 set -e
 
 echo "Installing Ansible  on CentOS..." 
- 
-
-sudo tee /etc/yum.repos.d/docker.repo <<-'EOF'
-[dockerrepo]
-name=Docker Repository
-baseurl=https://yum.dockerproject.org/repo/main/centos/7/
-enabled=1
-gpgcheck=1
-gpgkey=https://yum.dockerproject.org/gpg
-EOF
+su -c 'rpm -Uvh http://download.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-8.noarch.rpm'  
 
 yum -y install ansible
 yum -y install jq
